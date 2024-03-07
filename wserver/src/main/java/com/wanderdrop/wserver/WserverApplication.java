d1dbf32eb550a7abf1601f18c66bba0cbe3f0beb
Eleri Mets <eleri.apsolon@gmail.com>
2024-03-07 21:10:41 +0200

    Init

---
+++ wserver/src/main/java/com/wanderdrop/wserver/WserverApplication.java
@@ -0,0 +1,13 @@
+package com.wanderdrop.wserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WserverApplication.class, args);
+	}
+
+}
