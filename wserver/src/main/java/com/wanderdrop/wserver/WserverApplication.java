372af149a9095bf3e462a204ec9f71db39abe30e
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-06 16:21:33 +0300

    Create entity for DeletionReason.

--- wserver/src/main/java/com/wanderdrop/wserver/WserverApplication.java
+++ wserver/src/main/java/com/wanderdrop/wserver/WserverApplication.java
@@ -2,11 +2,14 @@ package com.wanderdrop.wserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com/wanderdrop/wserver/model")
 public class WserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WserverApplication.class, args);
 	}
 
