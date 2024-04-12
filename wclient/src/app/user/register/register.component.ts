afec11cb5c4cda3784d6273e579fa2e65ae3cd66
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-12 15:42:32 +0300

    Use creative form. Inside ngOnInit lifecycle hook initialize the form controls. Bind the registerForm FormGroup instance from the component class to the form element.

--- wclient/src/app/user/register/register.component.ts
+++ wclient/src/app/user/register/register.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      userPassword: new FormControl(null, Validators.required),
+      agreeToTerms: new FormControl(false, Validators.requiredTrue),
+    });
+  }
+
+  onSubmit() {}
 }
