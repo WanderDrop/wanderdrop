a875142f6792061cbf4f81336e811f897ba86a05
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-15 17:37:38 +0300

    Adding the basics html for add-comment component.

--- wclient/src/app/comment/add-comment/add-comment.component.ts
+++ wclient/src/app/comment/add-comment/add-comment.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-comment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-comment.component.html',
-  styleUrl: './add-comment.component.css'
+  styleUrl: './add-comment.component.css',
 })
 export class AddCommentComponent {
-
+  onSubmit() {}
 }
