4cbde27f2f61855f15b6da9c01969eb0e546fc33
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-15 14:31:27 +0300

    Implement comment-item component. Use it temporarily inside attraction view.

--- wclient/src/app/comment/comment-list/comment-item/comment-item.component.ts
+++ wclient/src/app/comment/comment-list/comment-item/comment-item.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Attraction } from '../../../attraction/attraction.model';
+import { Comment } from '../../comment.model';
 
 @Component({
   selector: 'app-comment-item',
   standalone: true,
   imports: [],
   templateUrl: './comment-item.component.html',
-  styleUrl: './comment-item.component.css'
+  styleUrl: './comment-item.component.css',
 })
-export class CommentItemComponent {
+export class CommentItemComponent implements OnInit {
+  comment: Comment | undefined;
 
+  ngOnInit(): void {
+    this.comment = new Comment(
+      1,
+      'Nice place',
+      'I fell in love with this place.'
+    );
+  }
 }
