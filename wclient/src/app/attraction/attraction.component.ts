80852e678594c1ce319d26cca944e7aa562651a6
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-15 22:35:20 +0300

    Display 'No comments about this attraction' when there are no comments about the attraction using *ngIf directive. Inject Comment service into Attraction component. Update comment array inside Attraction component when new comment is added by subscribing to the commentsUpdated Observable.

--- wclient/src/app/attraction/attraction.component.ts
+++ wclient/src/app/attraction/attraction.component.ts
@@ -6,6 +6,9 @@ import { CommentsComponent } from '../comment/comment.component';
 import { AddCommentComponent } from '../comment/add-comment/add-comment.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AttractionService } from './attraction.service';
+import { CommentService } from '../comment/comment.service';
+import { Comment } from '../comment/comment.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-attraction',
@@ -17,19 +20,28 @@ import { AttractionService } from './attraction.service';
     CommentListComponent,
     CommentsComponent,
     AddCommentComponent,
+    CommonModule,
   ],
 })
 export class AttractionComponent implements OnInit {
   attraction!: Attraction;
+  comments!: Comment[];
   @ViewChild('addCommentContent') addCommentContent!: TemplateRef<any>;
 
   constructor(
-    public modalService: NgbModal,
-    private attractionService: AttractionService
+    private modalService: NgbModal,
+    private attractionService: AttractionService,
+    private commentService: CommentService
   ) {}
 
   ngOnInit(): void {
     this.attraction = this.attractionService.getAttraction();
+    this.comments = this.commentService.getComments(this.attraction.id);
+    this.commentService
+      .getCommentsUpdated()
+      .subscribe((comments: Comment[]) => {
+        this.comments = comments;
+      });
   }
 
   onAddComment() {
