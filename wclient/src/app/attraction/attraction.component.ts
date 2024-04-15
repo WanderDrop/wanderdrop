9d03ac06a6dd1ae16b75273171155485ac21eed8
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-15 20:47:20 +0300

    Add ng-bootstrap to the project.

--- wclient/src/app/attraction/attraction.component.ts
+++ wclient/src/app/attraction/attraction.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { Attraction } from './attraction.model';
 import { CommentItemComponent } from '../comment/comment-list/comment-item/comment-item.component';
 import { CommentListComponent } from '../comment/comment-list/comment-list.component';
+import { CommentsComponent } from '../comment/comment.component';
+import { AddCommentComponent } from '../comment/add-comment/add-comment.component';
 
 @Component({
   selector: 'app-attraction',
   standalone: true,
   templateUrl: './attraction.component.html',
   styleUrl: './attraction.component.css',
-  imports: [CommentItemComponent, CommentListComponent],
+  imports: [
+    CommentItemComponent,
+    CommentListComponent,
+    CommentsComponent,
+    AddCommentComponent,
+  ],
 })
 export class AttractionComponent {
   attraction!: Attraction;
@@ -23,4 +30,6 @@ export class AttractionComponent {
     );
     console.log(this.attraction);
   }
+
+  onAddComment() {}
 }
