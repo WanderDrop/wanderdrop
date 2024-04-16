9e92eb64893731c361f52c1838389f8badc19334
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-16 22:43:35 +0300

    Create skeleton for modify-component. Add app-header and app-footer to app.component to make them visible.

---
+++ wclient/src/app/attraction/modify-attraction/modify-attraction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-attraction',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-attraction.component.html',
+  styleUrl: './modify-attraction.component.css'
+})
+export class ModifyAttractionComponent {
+
+}
