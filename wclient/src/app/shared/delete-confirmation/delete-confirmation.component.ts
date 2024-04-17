ecb56fd7797ab1eb76380238b205ccabdb6e8eee
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-17 16:13:14 +0300

    Add overall html to the delete-confirmation component.

--- wclient/src/app/shared/delete-confirmation/delete-confirmation.component.ts
+++ wclient/src/app/shared/delete-confirmation/delete-confirmation.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-delete-confirmation',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './delete-confirmation.component.html',
-  styleUrl: './delete-confirmation.component.css'
+  styleUrl: './delete-confirmation.component.css',
 })
 export class DeleteConfirmationComponent {
-
+  reasons: string[] = [];
+  onDelete() {}
+  onCancel() {}
 }
