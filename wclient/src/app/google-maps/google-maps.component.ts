5d527d7215767732e29a15f8661d28d22ec7fd55
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-27 11:49:22 +0300

    Add @angular/google-maps package to the project. Create skeleton component google-maps.

---
+++ wclient/src/app/google-maps/google-maps.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-maps',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-maps.component.html',
+  styleUrl: './google-maps.component.css'
+})
+export class GoogleMapsComponent {
+
+}
