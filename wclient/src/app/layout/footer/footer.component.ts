f3368ebf2c710b68b14f23eb37ff3ba19d5a7bb5
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-27 21:17:45 +0300

    Add temporary navigational buttons in footer for development. To be removed in future.

--- wclient/src/app/layout/footer/footer.component.ts
+++ wclient/src/app/layout/footer/footer.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  constructor(private router: Router) {}
 
+  openAttraction() {
+    this.router.navigate(['/attraction']);
+  }
+
+  openRegistrationForm() {
+    this.router.navigate(['/register']);
+  }
 }
