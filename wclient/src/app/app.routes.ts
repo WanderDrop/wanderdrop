bf2ed87e5cabb9356289bc8fee3438f1a1e0e64a
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-27 19:48:44 +0300

    Display Google maps. Add script to index.html.

--- wclient/src/app/app.routes.ts
+++ wclient/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './user/register/register.component';
 import { AttractionComponent } from './attraction/attraction.component';
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'attraction', component: AttractionComponent },
+  { path: 'home', component: GoogleMapsComponent },
 ];
--- wclient/src/app/google-maps/google-maps.component.ts
+++ wclient/src/app/google-maps/google-maps.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-google-maps',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './google-maps.component.html',
-  styleUrl: './google-maps.component.css'
+  styleUrl: './google-maps.component.css',
 })
 export class GoogleMapsComponent {
-
+  center = { lat: 58.391, lng: 24.495 };
 }
