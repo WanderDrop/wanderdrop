7bc207698c2f8dd40bc41a93b3224e861f1861c3
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-29 15:03:29 +0300

    Add initial logic to display dropdown menu in header.

--- wclient/src/app/layout/header/header.component.ts
+++ wclient/src/app/layout/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [ProfileDropdownComponent],
 })
 export class HeaderComponent {
   constructor(private router: Router) {}
