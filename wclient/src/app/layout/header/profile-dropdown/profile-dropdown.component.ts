f780ec26b486f8f0d68d8b1aaf33a282cb8748af
Eleri Mets <eleri.apsolon@gmail.com>
2024-04-29 20:15:01 +0300

    Resolve css glitch by adding conditional ngStyle. Code cleanup.

--- wclient/src/app/layout/header/profile-dropdown/profile-dropdown.component.ts
+++ wclient/src/app/layout/header/profile-dropdown/profile-dropdown.component.ts
@@ -18,8 +18,8 @@ import {
   styleUrl: './profile-dropdown.component.css',
 })
 export class ProfileDropdownComponent implements AfterViewInit {
-  @ViewChildren('smallDropdownMenu') ddmenusmall!: QueryList<ElementRef>;
-  @ViewChildren('largeDropdownMenu') ddmenularge!: QueryList<ElementRef>;
+  @ViewChildren('smallDropdownMenu') dropdownMenuSmall!: QueryList<ElementRef>;
+
   screenWidth: number = window.innerWidth;
 
   constructor(private renderer: Renderer2, private cdr: ChangeDetectorRef) {}
@@ -32,20 +32,15 @@ export class ProfileDropdownComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.ddmenusmall.changes.subscribe(() => {
-      this.adjustMenuClass();
-    });
-
-    this.ddmenularge.changes.subscribe(() => {
+    this.dropdownMenuSmall.changes.subscribe(() => {
       this.adjustMenuClass();
     });
   }
 
   adjustMenuClass() {
     const screenWidth = window.innerWidth;
-    console.log('Screen width: ', screenWidth);
 
-    this.ddmenusmall?.forEach((menu) => {
+    this.dropdownMenuSmall?.forEach((menu) => {
       const dropdownMenu = menu.nativeElement;
 
       if (
@@ -54,27 +49,10 @@ export class ProfileDropdownComponent implements AfterViewInit {
       ) {
         this.renderer.addClass(dropdownMenu, 'dropdown-menu-end');
         this.renderer.addClass(dropdownMenu, 'no-overlap');
-        console.log('Classes added');
       } else {
         this.renderer.removeClass(dropdownMenu, 'dropdown-menu-end');
         this.renderer.removeClass(dropdownMenu, 'no-overlap');
-        console.log('Classes removed');
       }
-
-      // Log the classes of the dropdown menu
-      console.log('Small dropdown menu classes: ', dropdownMenu.classList);
-    });
-
-    this.ddmenularge?.forEach((menu) => {
-      const dropdownMenu = menu.nativeElement;
-
-      if (screenWidth > 563 && screenWidth < 576) {
-        this.renderer.removeClass(dropdownMenu, 'dropdown-menu-end');
-        console.log('Class removed from large menu');
-      }
-
-      // Log the classes of the dropdown menu
-      console.log('Large dropdown menu classes: ', dropdownMenu.classList);
     });
   }
 
