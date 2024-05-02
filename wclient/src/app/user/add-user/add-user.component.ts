d9c453869ff3cf59c187082423b9150212dfa0c5
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-02 19:32:45 +0300

    Add images folder inside assets to hold images used inside the application. Add logic inside add-user component.

--- wclient/src/app/user/add-user/add-user.component.ts
+++ wclient/src/app/user/add-user/add-user.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.css'
+  styleUrl: './add-user.component.css',
 })
 export class AddUserComponent {
+  registerForm!: FormGroup;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      userPassword: new FormControl(null, Validators.required),
+      agreeToTerms: new FormControl(false, Validators.requiredTrue),
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  onNavigateHome() {
+    this.router.navigate(['/home']);
+  }
 }
