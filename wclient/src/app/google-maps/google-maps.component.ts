9fab1a91340b499938a5f21c943b35ae3181c3a8
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-02 23:28:49 +0300

    Start developing the logic of displaying the Marker. Print click event to the console.

--- wclient/src/app/google-maps/google-maps.component.ts
+++ wclient/src/app/google-maps/google-maps.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  GoogleMap,
+  GoogleMapsModule,
+  MapInfoWindow,
+  MapMarker,
+} from '@angular/google-maps';
 
 @Component({
   selector: 'app-google-maps',
   standalone: true,
-  imports: [GoogleMapsModule],
+  imports: [GoogleMapsModule, CommonModule],
   templateUrl: './google-maps.component.html',
   styleUrl: './google-maps.component.css',
 })
 export class GoogleMapsComponent implements OnInit {
+  markers: any = [];
+  infoContent = '';
   zoom = 12;
   center!: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
@@ -20,12 +28,44 @@ export class GoogleMapsComponent implements OnInit {
     minZoom: 8,
   };
 
+  @ViewChild(GoogleMap, { static: false })
+  map!: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info!: MapInfoWindow;
+
   ngOnInit() {
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       };
+    });
+  }
+
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()));
+  }
+
+  click(event: google.maps.MapMouseEvent) {
+    console.log(event);
+  }
+
+  openInfo(marker: any, content: string) {
+    this.infoContent = content;
+    this.info.open(marker);
+  }
+
+  addMarker() {
+    this.markers.push({
+      position: {
+        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      options: { animation: google.maps.Animation.BOUNCE },
     });
   }
 }
