52a9ca2a88e2936fac3a51037496dba1c117e53a
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-08 16:00:53 +0300

    Add user role enum with 3 roles. Use it inside user model.

--- wclient/src/app/user/user.model.ts
+++ wclient/src/app/user/user.model.ts
@@ -1,12 +1,14 @@
+import { UserRole } from './user-role.enum';
+
 export class User {
   private _userId!: number;
   private _email: string;
   private _password: string;
   private _firstName: string;
   private _lastName: string;
-  // private _roleId: UserRole;
+  private _roleId: UserRole;
   private _createdAt: Date;
-  private _updatedAt: Date;
+  private _updatedAt?: Date;
   private _status: string;
   public static lastId = 0;
 
@@ -14,7 +16,8 @@ export class User {
     email: string,
     password: string,
     firstName: string,
-    lastName: string
+    lastName: string,
+    roleId: UserRole = UserRole.User
   ) {
     User.lastId++;
     this._userId = User.lastId;
@@ -22,8 +25,8 @@ export class User {
     this._email = email;
     this._firstName = firstName;
     this._lastName = lastName;
+    this._roleId = roleId;
     this._createdAt = new Date();
-    this._updatedAt = new Date();
     this._status = 'active';
   }
 
@@ -63,11 +66,19 @@ export class User {
     this._lastName = value;
   }
 
+  get roleId(): UserRole {
+    return this._roleId;
+  }
+
+  set roleId(value: UserRole) {
+    this._roleId = value;
+  }
+
   get createdAt(): Date {
     return this._createdAt;
   }
 
-  get updatedAt(): Date {
+  get updatedAt(): Date | undefined {
     return this._updatedAt;
   }
 
