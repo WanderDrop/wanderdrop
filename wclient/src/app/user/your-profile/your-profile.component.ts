c0d162352d3b075a68f5e089b42d31b966027cde
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-09 09:45:45 +0300

    Set up the basic html for user profile page.

--- wclient/src/app/user/your-profile/your-profile.component.ts
+++ wclient/src/app/user/your-profile/your-profile.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-your-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './your-profile.component.html',
-  styleUrl: './your-profile.component.css'
+  styleUrl: './your-profile.component.css',
 })
 export class YourProfileComponent {
+  profileForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [{ value: '', disabled: true }, Validators.required],
+    });
+  }
+
+  changePassword() {}
+
+  close() {}
+
+  save() {}
 }
