eed4fd23825338fdb9be98645baafb364377fb86
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-13 10:57:54 +0300

    Add functionality to display user created attractions and comments.

--- wclient/src/app/user/your-activity/your-activity.component.ts
+++ wclient/src/app/user/your-activity/your-activity.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Attraction } from '../../attraction/attraction.model';
+import { Comment } from '../../comment/comment.model';
+import { AttractionService } from '../../attraction/attraction.service';
+import { CommentService } from '../../comment/comment.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-your-activity',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './your-activity.component.html',
-  styleUrl: './your-activity.component.css'
+  styleUrl: './your-activity.component.css',
 })
-export class YourActivityComponent {
+export class YourActivityComponent implements OnInit {
+  attractions: Attraction[] = [];
+  comments: Comment[] = [];
 
+  constructor(
+    private attractionService: AttractionService,
+    private commentService: CommentService
+  ) {}
+
+  ngOnInit(): void {
+    this.attractions = this.attractionService.attractions;
+    this.attractions.forEach((attraction) => {
+      const attractionComments = this.commentService.getComments(attraction.id);
+      this.comments = [...this.comments, ...attractionComments];
+    });
+  }
 }
