09defdd66198cb91f313df4850472fc6cb72b9db
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-17 15:46:20 +0300

    Add authentication classes. Include request and response models for authentication, user registration model, and an authentication service for handling user registration and authentication.

---
+++ wserver/src/main/java/com/wanderdrop/wserver/auth/AuthenticationService.java
@@ -0,0 +1,54 @@
+package com.wanderdrop.wserver.auth;
+
+import com.wanderdrop.wserver.config.JwtService;
+import com.wanderdrop.wserver.model.Role;
+import com.wanderdrop.wserver.model.Status;
+import com.wanderdrop.wserver.model.User;
+import com.wanderdrop.wserver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .status(Status.ACTIVE)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        System.out.println(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
