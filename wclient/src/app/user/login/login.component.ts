9ac27a5ff91508599138329dc31a403a9d92c22a
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-05-19 15:27:47 +0300

    Add login page logic to connect with server and add onRegister method to redirect to register page. Add disable functionality to the submit button.

--- wclient/src/app/user/login/login.component.ts
+++ wclient/src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,19 +10,21 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent { 
-  isSpinning: boolean = false;
+export class LoginComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+} 
   loginForm!: FormGroup;
+  registerForm: any;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private userService: UserService,
   ) {}
 
   ngOnInit() {
@@ -37,24 +39,27 @@ export class LoginComponent {
       console.log(res);
       if (res.userId != null) {
         const user = {
-          id: res.userId,
-          role: res.userRole
-        };
-        StorageService.saveUser(user);
-        StorageService.saveToken(res.jwt);
-        if (StorageService.isAdminLoggedIn()) {
-          this.router.navigateByUrl("/admin/home");
-        } else if (StorageService.isUserLoggedIn()) {
-          this.router.navigateByUrl("/user/home");
-        } else {
-          console.error("Bad credentials");
-        }
-      }
+        userId: res.userId,
+        role: res.role
+       };
+      StorageService.saveUser(user);
+      StorageService.saveToken(res.token);
+      if (StorageService.isAdminLoggedIn()) {
+        this.router.navigateByUrl("/home");
+      } else if (StorageService.isUserLoggedIn()) {
+        this.router.navigateByUrl("/home");
+      } else {
+      console.error("Bad credentials");
+      } 
+     }
     });
   }
 
   onNavigateHome() {
     this.router.navigate(['/home']);
   }
-}
 
+ onRegister(){
+  this.router.navigate(['/register']);
+ }
+}
