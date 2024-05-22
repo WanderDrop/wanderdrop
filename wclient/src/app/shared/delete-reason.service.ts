0039eaef6e5d457f8f6afa2c6f45d542a720f075
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-22 22:11:15 +0300

    Create subscriptions array inside deletion-reason and use ngOnDestroy method to unsubscribe all subscriptions when the service is destroyed.

--- wclient/src/app/shared/delete-reason.service.ts
+++ wclient/src/app/shared/delete-reason.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { StorageService } from '../user/storage/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DeleteReasonService {
+export class DeleteReasonService implements OnDestroy {
   private _reasons = new BehaviorSubject<
     { id: number; reasonMessage: string }[]
   >([]);
+  private subscriptions: Subscription[] = [];
 
   constructor(private http: HttpClient) {
     this.fetchReasons();
@@ -23,7 +24,7 @@ export class DeleteReasonService {
     const token = StorageService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    this.http
+    const sub = this.http
       .get<{ id: number; reasonMessage: string }[]>(
         'http://localhost:8080/api/deletion-reasons',
         { headers }
@@ -36,12 +37,14 @@ export class DeleteReasonService {
           console.error('Error:', error);
         },
       });
+    this.subscriptions.push(sub);
   }
 
   postReason(reason: string) {
     const token = StorageService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
+    // Return a response that includes the id and reasonMessage of the newly created reason
     return this.http.post<{ id: number; reasonMessage: string }>(
       'http://localhost:8080/api/deletion-reasons',
       { reasonMessage: reason },
@@ -51,5 +54,9 @@ export class DeleteReasonService {
 
   saveReasonToDatabase(reason: string) {
     this.postReason(reason);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
