877ccd03cf8817e04b25b852b9d858cb7786c5ba
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-22 11:20:09 +0300

    Manage subscriptions inside delete-confirmation component.

--- wclient/src/app/shared/delete-confirmation/delete-confirmation.component.ts
+++ wclient/src/app/shared/delete-confirmation/delete-confirmation.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { DeleteReasonService } from '../delete-reason.service';
 
 @Component({
@@ -12,10 +12,11 @@ import { DeleteReasonService } from '../delete-reason.service';
   templateUrl: './delete-confirmation.component.html',
   styleUrl: './delete-confirmation.component.css',
 })
-export class DeleteConfirmationComponent {
+export class DeleteConfirmationComponent implements OnDestroy {
   selectedReason: string = '';
   otherReason: string = '';
   reasons!: Observable<string[]>;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private activeModal: NgbActiveModal,
@@ -25,7 +26,7 @@ export class DeleteConfirmationComponent {
   }
 
   onDelete() {
-    this.reasons.subscribe((reasonsArray) => {
+    const reasonsSub = this.reasons.subscribe((reasonsArray) => {
       const reason =
         this.selectedReason !== 'other'
           ? this.selectedReason
@@ -38,9 +39,14 @@ export class DeleteConfirmationComponent {
       // this.deleteReasonService.saveReasonToDatabase(reason);
       this.activeModal.close({ action: 'delete', reasonId });
     });
+    this.subscriptions.push(reasonsSub);
   }
 
   onCancel() {
     this.activeModal.dismiss('cancel');
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
