2004e1e108407a4a8ede57a39bd9a2435414c8cc
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-27 15:54:40 +0300

    Add testUpdate and testDelete tests for testing DeletionReason Repository.

--- wserver/src/test/java/com/wanderdrop/wserver/repository/DeletionReasonRepositoryTest.java
+++ wserver/src/test/java/com/wanderdrop/wserver/repository/DeletionReasonRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DeletionReasonRepositoryTest {
 
     @Autowired
@@ -51,5 +51,38 @@ public class DeletionReasonRepositoryTest {
 
         assertNotNull(deletionReasons);
         assertFalse(deletionReasons.isEmpty());
+    }
+
+    @Test
+    public void testUpdate() {
+        DeletionReason deletionReason = new DeletionReason();
+        deletionReason.setDeletionReasonId(1L);
+        deletionReason.setReasonMessage("Test Reason");
+        deletionReason.setDefaultReason(false);
+
+        deletionReason = deletionReasonRepository.save(deletionReason);
+
+        deletionReason.setReasonMessage("Updated Test Reason");
+        deletionReasonRepository.save(deletionReason);
+
+        Optional<DeletionReason> updatedDeletionReason = deletionReasonRepository.findById(deletionReason.getDeletionReasonId());
+
+        assertTrue(updatedDeletionReason.isPresent());
+        assertEquals("Updated Test Reason", updatedDeletionReason.get().getReasonMessage());
+    }
+
+    @Test
+    public void testDelete() {
+        DeletionReason deletionReason = new DeletionReason();
+        deletionReason.setDeletionReasonId(1L);
+        deletionReason.setReasonMessage("Test Reason");
+        deletionReason.setDefaultReason(false);
+
+        deletionReason = deletionReasonRepository.save(deletionReason);
+        deletionReasonRepository.delete(deletionReason);
+
+        Optional<DeletionReason> deletedDeletionReason = deletionReasonRepository.findById(deletionReason.getDeletionReasonId());
+
+        assertFalse(deletedDeletionReason.isPresent());
     }
 }
