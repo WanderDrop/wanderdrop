c9f7b38c64165672f198c5d528a94482eec4cdae
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-27 11:42:27 +0300

    Create setup method with BeforeEach annotation to create a User and save it to the userRepository before each test is executed. Add additional tests to test attraction repository.

--- wserver/src/test/java/com/wanderdrop/wserver/repository/AttractionRepositoryTest.java
+++ wserver/src/test/java/com/wanderdrop/wserver/repository/AttractionRepositoryTest.java
@@ -3,8 +3,8 @@ package com.wanderdrop.wserver.repository;
 import com.wanderdrop.wserver.model.Attraction;
 import com.wanderdrop.wserver.model.Role;
 import com.wanderdrop.wserver.model.Status;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -12,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.wanderdrop.wserver.model.User;
 
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,20 +24,14 @@ public class AttractionRepositoryTest {
     @Autowired
     private AttractionRepository attractionRepository;
 
+    private User user;
+
     @Autowired
     private UserRepository userRepository;
 
-    @AfterEach
-    void tearDown() {
-        attractionRepository.deleteAll();
-        userRepository.deleteAll();
-    }
-
-    @Test
-    @Transactional
-    public void testSaveAttraction() {
-
-        User user = new User();
+    @BeforeEach
+    void setUp() {
+        user = new User();
         user.setEmail("testuser@test.com");
         user.setPassword("password");
         user.setFirstName("Firstname");
@@ -44,6 +40,46 @@ public class AttractionRepositoryTest {
         user.setStatus(Status.ACTIVE);
         user.setCreatedAt(new Timestamp(System.currentTimeMillis()));
         userRepository.save(user);
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        attractionRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindNonExistingAttraction() {
+        Optional<Attraction> found = attractionRepository.findById(999L);
+        assertThat(found).isEmpty();
+    }
+
+    @Test
+    public void testFindAttractionById() {
+        Attraction attraction = new Attraction(null, "Attraction 1", "Description 1", 22.1234, 16.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
+        attractionRepository.save(attraction);
+
+        Attraction found = attractionRepository.findById(attraction.getAttractionId()).orElse(null);
+        assertThat(found).isNotNull();
+        assertThat(found.getName()).isEqualTo("Attraction 1");
+    }
+
+    @Test
+    public void testGetAllAttractions() {
+        Attraction attraction1 = new Attraction(null, "Attraction 1", "Description 1", 22.1234, 16.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
+        Attraction attraction2 = new Attraction(null, "Attraction 2", "Description 2", 32.1234, 17.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
+        attractionRepository.save(attraction1);
+        attractionRepository.save(attraction2);
+
+        List<Attraction> attractions = attractionRepository.findAll();
+
+        assertThat(attractions).hasSize(2);
+        assertThat(attractions).contains(attraction1, attraction2);
+    }
+
+    @Test
+    public void testSaveAttraction() {
 
         Attraction attraction = new Attraction(null, "Attraction 1", "Description 1", 22.1234, 16.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
 
@@ -53,5 +89,31 @@ public class AttractionRepositoryTest {
         assertThat(found).isNotNull();
         assert found != null;
         assertThat(found.getName()).isEqualTo("Attraction 1");
+    }
+
+    @Test
+    public void testUpdateAttraction() {
+
+        Attraction attraction = new Attraction(null, "Attraction 1", "Description 1", 22.1234, 16.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
+        attractionRepository.save(attraction);
+
+        attraction.setName("Updated Attraction");
+        attractionRepository.save(attraction);
+
+        Attraction found = attractionRepository.findById(attraction.getAttractionId()).orElse(null);
+        assertThat(found).isNotNull();
+        assertThat(found.getName()).isEqualTo("Updated Attraction");
+    }
+
+    @Test
+    public void testDeleteAttraction() {
+
+        Attraction attraction = new Attraction(null, "Attraction 1", "Description 1", 22.1234, 16.3545, user, user, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), Status.ACTIVE, null);
+        attractionRepository.save(attraction);
+
+        attractionRepository.delete(attraction);
+
+        Optional<Attraction> found = attractionRepository.findById(attraction.getAttractionId());
+        assertThat(found).isEmpty();
     }
 }
