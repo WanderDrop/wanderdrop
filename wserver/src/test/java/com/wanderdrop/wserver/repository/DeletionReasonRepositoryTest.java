5fdf29d00a3d13027085bfb7a43bdfad21b339ec
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-27 15:48:42 +0300

    Add testSaveAndGetById and testFindAll tests for testing DeletionReason Repository.

--- wserver/src/test/java/com/wanderdrop/wserver/repository/DeletionReasonRepositoryTest.java
+++ wserver/src/test/java/com/wanderdrop/wserver/repository/DeletionReasonRepositoryTest.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -25,5 +28,28 @@ public class DeletionReasonRepositoryTest {
         deletionReason = deletionReasonRepository.save(deletionReason);
 
         assertNotNull(deletionReasonRepository.findById(deletionReason.getDeletionReasonId()));
+    }
+
+    @Test
+    public void testSaveAndGetById() {
+        DeletionReason deletionReason = new DeletionReason();
+        deletionReason.setDeletionReasonId(1L);
+        deletionReason.setReasonMessage("Test Reason");
+        deletionReason.setDefaultReason(false);
+
+        deletionReason = deletionReasonRepository.save(deletionReason);
+
+        Optional<DeletionReason> foundDeletionReason = deletionReasonRepository.findById(deletionReason.getDeletionReasonId());
+
+        assertTrue(foundDeletionReason.isPresent());
+        assertEquals(deletionReason, foundDeletionReason.get());
+    }
+
+    @Test
+    public void testFindAll() {
+        List<DeletionReason> deletionReasons = deletionReasonRepository.findAll();
+
+        assertNotNull(deletionReasons);
+        assertFalse(deletionReasons.isEmpty());
     }
 }
