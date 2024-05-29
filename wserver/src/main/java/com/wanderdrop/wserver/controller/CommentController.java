6e2edd1547a49a1430c259b27af71d980c367ab7
Eleri Mets <eleri.apsolon@gmail.com>
2024-05-29 12:17:20 +0300

    Update security configuration to handle the comment endpoints.

---
+++ wserver/src/main/java/com/wanderdrop/wserver/controller/CommentController.java
@@ -0,0 +1,44 @@
+package com.wanderdrop.wserver.controller;
+
+import com.wanderdrop.wserver.dto.CommentDto;
+import com.wanderdrop.wserver.service.comment.CommentService;
+import com.wanderdrop.wserver.service.comment.CommentServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<CommentDto> getAllActiveComments() {
+        return commentService.getAllActiveComments();
+    }
+
+    @GetMapping("/{id}")
+    public CommentDto getCommentById(@PathVariable Long id) {
+        return commentService.getCommentById(id);
+    }
+
+    @PostMapping
+    public CommentDto createComment(@RequestBody CommentDto commentDto) {
+        return commentService.createComment(commentDto);
+    }
+
+    @PutMapping("/{id}")
+    public CommentDto updateComment(@PathVariable Long id, @RequestBody CommentDto commentDto) {
+        return commentService.updateComment(id, commentDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable Long id, @RequestBody String deletionReason) {
+        commentService.deleteComment(id, deletionReason);
+    }
+}
