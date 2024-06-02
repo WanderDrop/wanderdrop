56ba8841326303157bd04dbbf66a91328a8bb4f6
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-02 05:10:15 +0300

    Add Report Mapper

--- wserver/src/main/java/com/wanderdrop/wserver/mapper/ReportMapper.java
+++ wserver/src/main/java/com/wanderdrop/wserver/mapper/ReportMapper.java
@@ -1,9 +1,8 @@
 package com.wanderdrop.wserver.mapper;
 
 import com.wanderdrop.wserver.dto.ReportDto;
-
-import com.wanderdrop.wserver.model.Attraction;
 import com.wanderdrop.wserver.model.Report;
+import com.wanderdrop.wserver.model.Status;
 import com.wanderdrop.wserver.model.User;
 import com.wanderdrop.wserver.repository.UserRepository;
 import org.springframework.stereotype.Component;
@@ -15,29 +14,25 @@ public class ReportMapper {
                 report.getReportId(),
                 report.getReportHeading(),
                 report.getReportMessage(),
-                report.getStatus(),
-                report.getAttractionId() != null ? report.getAttractionId().getAttractionId() : null,
+                // report.getStatus(),
+                report.getAttraction() != null ? report.getAttraction().getAttractionId() : null,
                 report.getCreatedBy() != null ? report.getCreatedBy().getUserId() : null,
                 report.getCreatedAt()
         );
     }
-public Report mapToReport(ReportDto reportDto, UserRepository userRepository) {
-    Report report = new Report();
-    report.setReportId(reportDto.getReportId());
-    report.setReportHeading(reportDto.getReportHeading());
-    report.setReportMessage(reportDto.getReportMessage());
-    report.setStatus(reportDto.getStatus());
-    if (reportDto.getAttractionId() != null) {
-        Attraction attraction = new Attraction();
-        attraction.setAttractionId(reportDto.getAttractionId());
-    }
-    report.setStatus(reportDto.getStatus());
-    if (reportDto.getCreatedBy() != null) {
-        User createdBy = userRepository.findById(reportDto.getCreatedBy()).orElse(null);
-        report.setCreatedBy(createdBy);
-    }
-    report.setCreatedAt(reportDto.getCreatedAt());
 
-    return report;
-}
+    public Report mapToReport(ReportDto reportDto, UserRepository userRepository) {
+        Report report = new Report();
+        report.setReportId(reportDto.getReportId());
+        report.setReportHeading(reportDto.getReportHeading());
+        report.setReportMessage(reportDto.getReportMessage());
+        report.setStatus(Status.ACTIVE);
+        if (reportDto.getCreatedBy() != null) {
+            User createdBy = userRepository.findById((reportDto.getCreatedBy())).orElse(null);
+            report.setCreatedBy(createdBy);
+        }
+        report.setCreatedAt(reportDto.getCreatedAt());
+
+        return report;
     }
+}
