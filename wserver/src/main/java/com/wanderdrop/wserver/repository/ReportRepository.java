8db96462798bca3d28fa8d35803770639dff93c5
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-02 05:11:00 +0300

    Add method in report repository to find reports by status and attraction id

--- wserver/src/main/java/com/wanderdrop/wserver/repository/ReportRepository.java
+++ wserver/src/main/java/com/wanderdrop/wserver/repository/ReportRepository.java
@@ -1,9 +1,13 @@
 package com.wanderdrop.wserver.repository;
 
 import com.wanderdrop.wserver.model.Report;
+import com.wanderdrop.wserver.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByStatusAndAttraction_AttractionId(Status status, Long attractionId);
 }
