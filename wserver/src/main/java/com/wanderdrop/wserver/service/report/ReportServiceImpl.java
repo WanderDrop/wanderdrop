abe0cac3d35fbf03189c18160f197fc71e9f8429
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-02 05:12:04 +0300

    add logic to report service impl

--- wserver/src/main/java/com/wanderdrop/wserver/service/report/ReportServiceImpl.java
+++ wserver/src/main/java/com/wanderdrop/wserver/service/report/ReportServiceImpl.java
@@ -3,6 +3,7 @@ package com.wanderdrop.wserver.service.report;
 import com.wanderdrop.wserver.dto.ReportDto;
 import com.wanderdrop.wserver.mapper.ReportMapper;
 import com.wanderdrop.wserver.model.*;
+import com.wanderdrop.wserver.repository.AttractionRepository;
 import com.wanderdrop.wserver.repository.ReportRepository;
 import com.wanderdrop.wserver.repository.UserRepository;
 import org.springframework.security.access.AccessDeniedException;
@@ -20,23 +21,26 @@ public class ReportServiceImpl implements ReportService {
     private final UserRepository userRepository;
     private final ReportRepository reportRepository;
     private final ReportMapper reportMapper;
+    private final AttractionRepository attractionRepository;
 
-    public ReportServiceImpl(UserRepository userRepository, ReportRepository reportRepository, ReportMapper reportMapper) {
+    public ReportServiceImpl(UserRepository userRepository, ReportRepository reportRepository, ReportMapper reportMapper, AttractionRepository attractionRepository) {
         this.userRepository = userRepository;
         this.reportRepository = reportRepository;
         this.reportMapper = reportMapper;
+        this.attractionRepository = attractionRepository;
     }
 
     @Override
-    public ReportDto saveReport (ReportDto reportDto) {
-
+    public ReportDto createReport(ReportDto reportDto, Long attractionId) {
         User currentUser = getCurrentAuthenticatedUser();
         if (currentUser == null || (currentUser.getRole() != Role.ADMIN && currentUser.getRole() != Role.USER)) {
             throw new AccessDeniedException("Only logged in users and admins can create an attraction.");
         }
+        Attraction attraction = attractionRepository.findById(attractionId)
+                .orElseThrow(() -> new IllegalArgumentException("Attraction with id " + attractionId + " not found"));
 
-        reportDto.setCreatedBy(currentUser.getUserId());
         Report report = reportMapper.mapToReport(reportDto, userRepository);
+        report.setAttraction(attraction);
         report.setCreatedBy(currentUser);
         report.setStatus(Status.ACTIVE);
         report.setCreatedAt(new Timestamp(System.currentTimeMillis()));
@@ -51,10 +55,18 @@ public class ReportServiceImpl implements ReportService {
     }
 
     @Override
-    public List<ReportDto> getAllReports() {
-        return reportRepository.findAll().stream()
-                .filter(report -> report.getStatus() == Status.ACTIVE)
-                .map(reportMapper::mapToReportDto)
+    public List<ReportDto> getAllActiveReports(Long attractionId) {
+        return reportRepository.findByStatusAndAttraction_AttractionId(Status.ACTIVE, attractionId)
+                .stream()
+                .map(reportMapper :: mapToReportDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ReportDto> getAllClosedReports(Long attractionId) {
+        return reportRepository.findByStatusAndAttraction_AttractionId(Status.DELETED, attractionId)
+                .stream()
+                .map(reportMapper :: mapToReportDto)
                 .collect(Collectors.toList());
     }
 
@@ -62,7 +74,7 @@ public class ReportServiceImpl implements ReportService {
     public void deleteReport(Long reportId) {
 
         Optional<Report> optionalReport = reportRepository.findById(reportId);
-        if (optionalReport.isPresent() && optionalReport.get().getStatus() == Status.ACTIVE) {
+        if (optionalReport.isPresent() && optionalReport.get().getStatus() == Status.ACTIVE && checkAdminUser()) {
             Report report = optionalReport.get();
             report.setStatus(Status.DELETED);
 
@@ -75,6 +87,14 @@ public class ReportServiceImpl implements ReportService {
     private User getCurrentAuthenticatedUser() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByEmail(username).orElse(null);
+    }
+
+    private boolean checkAdminUser() {
+        User currentUser = getCurrentAuthenticatedUser();
+        if (currentUser == null || currentUser.getRole() != Role.ADMIN) {
+            throw new AccessDeniedException("Only admins can perform this action.");
+        }
+        return true;
     }
 
 }
