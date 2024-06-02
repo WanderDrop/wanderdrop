a0bf0b35b2ca30a044055df049cbf67b5bd18727
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-02 05:03:20 +0300

    Modify logic of add new report page

--- wclient/src/app/report-page/add-new-report-page/add-new-report-page.component.ts
+++ wclient/src/app/report-page/add-new-report-page/add-new-report-page.component.ts
@@ -1,86 +1,67 @@
-import { Component, EventEmitter, Input, NgZone, OnDestroy, OnInit, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, NgZone, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../user/user.service';
 import { ReportPage } from '../report-page.model';
 import { ReportPageService } from '../report-page.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AttractionComponent } from '../../attraction/attraction.component';
+import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-new-report-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-new-report-page.component.html',
   styleUrl: './add-new-report-page.component.css',
 })
-export class AddNewReportPageComponent implements OnDestroy {
+export class AddNewReportPageComponent {
   @Input() reportMessage: string = '';
   @Input() attractionId!: number;
   @Input() attractionName: string = '';
   @Output() dataChanged = new EventEmitter<{
-    attractionId: AttractionComponent["id"];
     reportHeading: AttractionComponent["attractionName"];
     reportMessage: string;
   }>();
   private subscriptions: Subscription[] = [];
-
   constructor(
     private router: Router,
+    private reportService: ReportPageService,
     private ngZone: NgZone,
-    private modalService: NgbModal,
-    private reportPageService: ReportPageService,
-    private userService: UserService
-  ) { }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
-  }
-
+    private userService: UserService,
+    private modalService: NgbModal
+  ){}
   onClose() {
     this.ngZone.run(() => {
       this.router.navigate(['/home']);
     });
   }
-
-  closeModal() {
-    this.modalService.dismissAll();
-  }
-
-  onSubmit() {
-    const reportPage = new ReportPage(
-      this.attractionId,
-      this.attractionName,
-      this.reportMessage,
-      this.userService.getDummyUser().UserId
-    );
-    this.reportPageService.addReport(reportPage);
-    this.modalService.dismissAll();
-  }
-
-  onCancel() {
-    this.modalService.dismissAll();
-  }
-  onAddReportPage() {
+  onAddReport() {
     const userId = this.userService.getDummyUser().UserId;
 
     const newReport = new ReportPage(
-      this.attractionId,
       this.attractionName,
       this.reportMessage,
+      this.attractionId,
       userId
     );
-    const addReportPageSub = this.reportPageService
-      .addReport(newReport)
+
+    const addReportSub = this.reportService
+      .addReport(this.attractionId, newReport)
       .subscribe({
-        next: (response) => {
-          this.reportPageService.reports.push(response);
+        next: (response)=>{
+          this.reportService.reports.push(response);
         },
-        error: (error) => {
-          console.error("Error adding new report", error);
+          error: (error) => { console.error('Error adding new report:', error);
         },
-      })
-    this.subscriptions.push(addReportPageSub);
+      });
+      this.subscriptions.push(addReportSub);
+      console.log("Report done");
+      this.modalService.dismissAll();
+  }
+  onCancel() {
+    this.modalService.dismissAll();
   }
 }
