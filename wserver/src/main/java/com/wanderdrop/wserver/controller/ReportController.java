c36f67d27c8e523bc02b9db2f0cc974726f682e8
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-02 05:08:50 +0300

    Add logic to do request

--- wserver/src/main/java/com/wanderdrop/wserver/controller/ReportController.java
+++ wserver/src/main/java/com/wanderdrop/wserver/controller/ReportController.java
@@ -1,36 +1,42 @@
 package com.wanderdrop.wserver.controller;
 
 import com.wanderdrop.wserver.dto.ReportDto;
-import com.wanderdrop.wserver.service.report.ReportServiceImpl;
+import com.wanderdrop.wserver.service.report.ReportService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@RestController
-@RequestMapping("/api/reports")
-public class ReportController {
-    private final ReportServiceImpl reportServiceImpl;
 
-    public ReportController(ReportServiceImpl reportServiceImpl) {
-        this.reportServiceImpl = reportServiceImpl;
+@RestController
+@RequestMapping("/api/reportes")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+    @GetMapping("/attraction/{attractionId}/active")
+    public List<ReportDto> getAllActiveReports(@PathVariable Long attractionId) {
+        return reportService.getAllActiveReports(attractionId);
     }
 
-    @GetMapping
-    public List<ReportDto> getAllReports() {
-        return reportServiceImpl.getAllReports();
+    @GetMapping("/attraction/{attractionId}/closed")
+    public List<ReportDto> getAllClosedReports(@PathVariable Long attractionId) {
+        return reportService.getAllClosedReports(attractionId);
     }
 
     @GetMapping("/{id}")
     public ReportDto getReportById(@PathVariable Long id) {
-        return reportServiceImpl.getReportById(id);
+        return reportService.getReportById(id);
     }
 
-    @PostMapping
-    public ReportDto createReport(@RequestBody ReportDto reportDto) {
-        return reportServiceImpl.saveReport(reportDto);
+    @PostMapping("/{attractionId}")
+    public ReportDto createReport(@RequestBody ReportDto reportDto,@PathVariable Long attractionId ) {
+        return reportService.createReport(reportDto, attractionId);
     }
 
     @PutMapping("/{id}")
     public void deleteReport (@PathVariable Long id) {
-        reportServiceImpl.deleteReport(id);
+        reportService.deleteReport(id);
     }
 }
