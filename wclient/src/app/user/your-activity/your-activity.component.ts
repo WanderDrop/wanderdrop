616091cc484487b2f153c280e3155c710b91c615
Eleri Mets <eleri.apsolon@gmail.com>
2024-06-04 11:52:48 +0300

    Add the possibility for the user to select if they want to see all, active, or deleted attractions and comments.

--- wclient/src/app/user/your-activity/your-activity.component.ts
+++ wclient/src/app/user/your-activity/your-activity.component.ts
@@ -5,17 +5,20 @@ import { AttractionService } from '../../attraction/attraction.service';
 import { CommentService } from '../../comment/comment.service';
 import { CommonModule } from '@angular/common';
 import { switchMap, map, forkJoin, Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-your-activity',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './your-activity.component.html',
   styleUrl: './your-activity.component.css',
 })
 export class YourActivityComponent implements OnInit {
   attractions: Attraction[] = [];
   comments: Comment[] = [];
+  attractionFilter: string = 'ALL';
+  commentFilter: string = 'ALL';
 
   constructor(
     private attractionService: AttractionService,
