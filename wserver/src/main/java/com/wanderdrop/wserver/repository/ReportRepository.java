80b51af5d4309a79820348f2dc438ae0ca7f6cfb
AngieSepulveda <147972715+AngieSepulveda@users.noreply.github.com>
2024-06-08 17:20:27 +0300

    Add backend logic to get all reports and display list sorted by active and closed

--- wserver/src/main/java/com/wanderdrop/wserver/repository/ReportRepository.java
+++ wserver/src/main/java/com/wanderdrop/wserver/repository/ReportRepository.java
@@ -1,6 +1,7 @@
 package com.wanderdrop.wserver.repository;
 import com.wanderdrop.wserver.model.Report;
 import com.wanderdrop.wserver.model.Status;
+import com.wanderdrop.wserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
     List<Report> findByStatusAndAttraction_AttractionId(Status status, Long attractionId);
+    List<Report> findByStatus(Status status);
+    List<Report> findByCreatedBy(User user);
 }
--- wserver/src/main/java/com/wanderdrop/wserver/service/report/ReportService.java
+++ wserver/src/main/java/com/wanderdrop/wserver/service/report/ReportService.java
@@ -8,7 +8,9 @@ public interface ReportService {
     ReportDto createReport(ReportDto reportDto, Long attractionId);
     ReportDto getReportById(Long reportId);
     List<ReportDto> getAllActiveReports(Long attractionId);
+    List<ReportDto> getAllActiveReports();
     List<ReportDto> getAllClosedReports(Long attractionId);
+    List<ReportDto> getAllClosedReports();
     void deleteReport(Long reportId);
 
 }
